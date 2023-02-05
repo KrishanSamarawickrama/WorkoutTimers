0fdf21892e9faf853225bc108abc9fe3e2e7953e
Krishan Samarawickrama <Krishans1990@live.com>
2023-02-05 23:24:00 +0530

    Passing navigation data

--- WorkoutTimers.App/AppShell.xaml.cs
+++ WorkoutTimers.App/AppShell.xaml.cs
@@ -1,10 +1,28 @@
-﻿namespace WorkoutTimers.App
+﻿using System.Windows.Input;
+
+namespace WorkoutTimers.App
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+            Routing.RegisterRoute("Settings", typeof(Views.Settings));
+
+            NavigateToSettingsCommand = new Command(async () =>
+            {
+                //await DisplayAlert("Menue Item", "Settings selected", "Ok");
+                await GoToAsync("Settings",
+                        new Dictionary<string, object>
+                        {
+                            {"Message","I'm Batman" }
+                        }
+                    );
+                this.FlyoutIsPresented = this.FlyoutBehavior != FlyoutBehavior.Flyout;
+            });
+            BindingContext = this;
         }
+
+        public ICommand NavigateToSettingsCommand { get; private set; }
     }
 }

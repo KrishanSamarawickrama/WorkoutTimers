a641c9a95d2e8356a4625a514194cc853d24d516
Krishan Samarawickrama <Krishans1990@live.com>
2023-02-05 13:47:35 +0530

    Initial commit

---
+++ WorkoutTimers.App/MauiProgram.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using WorkoutTimers.App.Views;
+using WorkoutTimers.Lib.Services;
+using WorkoutTimers.Lib.ViewModels;
+
+namespace WorkoutTimers.App
+{
+	public static class MauiProgram
+	{
+		public static MauiApp CreateMauiApp()
+		{
+			var builder = MauiApp.CreateBuilder();
+			builder
+				.UseMauiApp<App>()
+				.ConfigureFonts(fonts =>
+				{
+					fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+					fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+				})
+				.Services
+				.AddSingleton<IIntervalService,InMemoryIntervalService>()
+				.AddSingleton<RegularIntervalModel>()
+				.AddTransient<NewRegularIntervalModel>()
+				.AddSingleton<StaggeredIntervalModel>()
+				.AddTransient<NewStaggeredIntervalModel>()
+				.AddSingleton<SettingsModel>()
+				.AddSingleton<StaggeredIntervals>()
+				.AddSingleton<RegularIntervals>()
+				.AddTransient<NewStaggeredInterval>()
+				.AddTransient<NewRegularInterval>()
+				.AddSingleton<Settings>();
+
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+			return builder.Build();
+		}
+	}
+}
